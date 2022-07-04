0d7c4740310bc9623d35cd6af9c5d4b756dd0a88
martinmcshane <martinmcshane@gmail.com>
2022-07-04 21:32:44 +0100

    edited on laptop to make it cool AF

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,12 @@
 #first python script uploaded to git
 # a geordie hello world
 
-print("alreeeeeet kidda")
+def greeting(are_they_geet_canny_as_owt):
+    if are_they_geet_canny_as_owt == "y":
+        print("Alreet Kidda")
+    else:
+        print("Hello World")
+
+shizzle = input("are you a Geordie? y/n ")
+
+greeting(shizzle)
